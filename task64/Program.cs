74af97b1f2511bc9b2b1808723899268db8339b0
Dmitry Mordanov <mordanov314@gmail.com>
2022-04-12 15:31:52 +0300

    initial and final commit

---
+++ task64/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от M до N в обратном порядке. M = 1; N = 5. -> «5, 4, 3, 2, 1»
+
+using System;
+
+void PrintNatur(int m,int n)    // Рекурсивная функция, которая печатает очередное число из промежутка, 
+{                               // и вызывает сама себя для печати следующего числа
+  if (m>=n)  // вдруг введут первое число больше второго
+  {
+    Console.WriteLine(n);  // 
+  }
+  else
+  {
+    Console.WriteLine(n);
+    PrintNatur(m,n-1);
+  }
+}
+
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+PrintNatur(M,N);
---
+++ task66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N. M = 1; N = 15 -> 120 M = 4; N = 8. -> 30
+
+using System;
+
+int Summ(int m,int n)    // Рекурсивная функция, которая суммирует числа в промежутке m..n
+{
+    if (m>n)  // вдруг введут первое число больше второго
+    {
+        return 0;
+    }
+    else
+    {
+        return n + Summ(m,n-1);
+    }
+}
+
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма натуральных чисел в промежутке [{M}..{N}] равна {Summ(M,N)}");
